ccbf8e7900b237d5edcd409bb27027eabeb26c23
Exfar Nanda Micola <exfarnanda1945@gmail.com>
2022-09-19 19:55:37 +0700

    fix: change fragment to FragmentContainerView and add navigate up

--- app/src/main/java/com/example/crudretrofit/MainActivity.kt
+++ app/src/main/java/com/example/crudretrofit/MainActivity.kt
@@ -1,21 +1,23 @@
 package com.example.crudretrofit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.crudretrofit.databinding.ActivityMainBinding
-import com.example.crudretrofit.repository.PostRepository
-import com.example.crudretrofit.viewmodel.MainViewModel
-import com.example.crudretrofit.viewmodel.MainViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupActionBarWithNavController(findNavController(R.id.fragmentContainerView))
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
