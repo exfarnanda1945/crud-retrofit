5579a0e6a0b03277b440af561be1eb2b0b1df849
Exfar Nanda Micola <exfarnanda1945@gmail.com>
2022-09-19 11:42:31 +0700

    fix: host config

--- app/src/main/java/com/example/crudretrofit/utils/Constants.kt
+++ app/src/main/java/com/example/crudretrofit/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.example.crudretrofit.utils
 
 class Constants {
     companion object{
-        private const val HOST = "172.16.8.85"
+        private const val HOST = "172.16.8.41"
         private const val PORT = 5050
        const val BASE_URL = "http://$HOST:$PORT"
     }
