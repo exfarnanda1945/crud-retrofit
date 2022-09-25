3320472c31c974a85434a74147da01e5c685c9a0
Exfar Nanda Micola <exfarnanda1945@gmail.com>
2022-09-25 16:55:24 +0700

    fix: implement custom response

--- app/src/main/java/com/example/crudretrofit/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/crudretrofit/viewmodel/MainViewModel.kt
@@ -4,42 +4,55 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.crudretrofit.api.ApiResponse
 import com.example.crudretrofit.models.IdModel
 import com.example.crudretrofit.models.PostModel
+import com.example.crudretrofit.models.PostModelDelete
 import com.example.crudretrofit.repository.PostRepository
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
+//shared viewmodel
 class MainViewModel(private val repo: PostRepository) : ViewModel() {
-    private var _response: MutableLiveData<Response<List<PostModel>>> = MutableLiveData()
-    val response: LiveData<Response<List<PostModel>>> get() = _response
-
-    fun list() {
+    fun list():LiveData<ApiResponse<List<PostModel>>> {
+        val result = MutableLiveData<ApiResponse<List<PostModel>>>()
         viewModelScope.launch {
             val res = repo.list()
-            _response.value = res
+            result.postValue(res)
         }
+
+        return result
     }
 
-    fun post(post: PostModel) {
+    // https://stackoverflow.com/questions/60910978/how-to-return-value-from-async-coroutine-scope-such-as-viewmodelscope-to-your-ui
+    fun post(post: PostModel):LiveData<ApiResponse<PostModel>> {
+        val result = MutableLiveData<ApiResponse<PostModel>>()
         viewModelScope.launch {
-            repo.post(post)
-
+           val response= repo.post(post)
+            result.postValue(response)
         }
+
+        return result
     }
 
-    fun delete(id: IdModel) {
+    fun delete(id: IdModel):LiveData<ApiResponse<PostModelDelete>> {
+        val result = MutableLiveData<ApiResponse<PostModelDelete>>()
         viewModelScope.launch {
-            repo.delete(id)
-
+            val respon =             repo.delete(id)
+            result.postValue(respon)
         }
+
+        return result
     }
 
-    fun update(post: PostModel) {
+    fun update(post: PostModel):LiveData<ApiResponse<Unit>> {
+        val result = MutableLiveData<ApiResponse<Unit>>()
         viewModelScope.launch {
-            repo.update(post)
-
+            val respon = repo.update(post)
+            result.postValue(respon)
         }
+
+        return result
     }
 
 }
